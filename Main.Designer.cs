aa3994b29e7110ba207eb664a52130087dcc728e
bigzyute <orainemc@gmail.com>
2019-05-10 18:50:18 -0500

    Update

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -162,6 +162,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(1213, 624);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
